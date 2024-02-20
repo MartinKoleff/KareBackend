8d16887a7a0f373db0c0d60db1c62cd66a06cfdd
MartinKoleff <martin_koleff@abv.bg>
2024-02-20 23:47:36 +0200

    Initial commit

---
+++ src/main/java/com/koleff/kare/KareApplication.java
@@ -0,0 +1,13 @@
+package com.koleff.kare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KareApplication.class, args);
+	}
+
+}
