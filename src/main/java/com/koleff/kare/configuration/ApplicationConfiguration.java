dec23d9512685b9d8b067f887dcee1d9a3460838
MartinKoleff <martin_koleff@abv.bg>
2024-02-22 22:26:26 +0200

    Added DB initializer configuration. Added role and user repositories.

---
+++ src/main/java/com/koleff/kare/configuration/ApplicationConfiguration.java
@@ -0,0 +1,55 @@
+package com.koleff.kare.configuration;
+
+import com.koleff.kare.models.entity.Role;
+import com.koleff.kare.models.entity.User;
+import com.koleff.kare.repository.RoleRepository;
+import com.koleff.kare.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Configuration
+public class ApplicationConfiguration {
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationConfiguration(RoleRepository roleRepository,
+                                    UserRepository userRepository,
+                                    PasswordEncoder passwordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /**
+     * Always add Admin user and all roles in the DB
+     */
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+
+            //DB is initialized
+            if (roleRepository.findByAuthority("ADMIN").isPresent()) return;
+
+            Role adminRole = roleRepository.save(new Role("ADMIN"));
+            roleRepository.save(new Role("USER"));
+
+            Set<Role> roles = new HashSet<>();
+            roles.add(adminRole);
+
+            User admin = new User(UUID.randomUUID().toString(),
+                    "admin",
+                    passwordEncoder.encode("password"),
+                    "admin@admin.com",
+                    roles);
+
+            userRepository.save(admin);
+        };
+    }
+}
--- src/main/java/com/koleff/kare/repository/UserRepository.java
+++ src/main/java/com/koleff/kare/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.koleff.kare.repository;
 import com.koleff.kare.models.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
