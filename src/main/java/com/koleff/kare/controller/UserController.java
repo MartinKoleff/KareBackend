252c82a544c6aca823810ebac27f1896cfc25ade
MartinKoleff <martin_koleff@abv.bg>
2024-02-22 22:38:17 +0200

    Added new endpoint for testing User authentication.

--- src/main/java/com/koleff/kare/controller/UserController.java
+++ src/main/java/com/koleff/kare/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.koleff.kare.controller;
 
+import com.koleff.kare.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class UserController {
 
-    @GetMapping("/")
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/test")
     public String testUserAccess(){
         return "User access level";
     }
-    
+
+    @GetMapping("/test2")
+    public UserDetails testUserAccess2(){
+        return userService.loadUserByUsername("Koleff");
+    }
 }
