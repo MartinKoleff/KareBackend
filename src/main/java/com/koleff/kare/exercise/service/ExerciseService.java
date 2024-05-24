467c880a676d68325f06b7b8ebef4df730e23606
MartinKoleff <martin_koleff@abv.bg>
2024-05-24 15:53:15 +0300

    Added service interfaces for exercise and exercise set. Added mappers to impl services.

--- src/main/java/com/koleff/kare/exercise/service/ExerciseService.java
+++ src/main/java/com/koleff/kare/exercise/service/ExerciseService.java
@@ -1,64 +1,17 @@
 package com.koleff.kare.exercise.service;
 
-import com.koleff.kare.common.Constants;
+import com.koleff.kare.exercise.models.dto.ExerciseDto;
 import com.koleff.kare.exercise.models.entity.Exercise;
 import com.koleff.kare.exercise.models.entity.ExerciseSet;
-import com.koleff.kare.exercise.repository.ExerciseRepository;
-import com.koleff.kare.exercise.repository.ExerciseSetRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
 
-@Service
-public class ExerciseService {
-    private final ExerciseRepository exerciseRepository;
+public interface ExerciseService {
 
-    private final ExerciseSetRepository exerciseSetRepository;
-
-    @Autowired
-    public ExerciseService(
-            ExerciseRepository exerciseRepository,
-            ExerciseSetRepository exerciseSetRepository
-    ) {
-        this.exerciseRepository = exerciseRepository;
-        this.exerciseSetRepository = exerciseSetRepository;
-    }
-
-    public List<Exercise> getCatalogExercises(Integer muscleGroupId) {
-        return exerciseRepository.findByWorkoutIdAndMuscleGroupId(Constants.CATALOG_WORKOUT_ID, muscleGroupId);
-    }
-
-    public List<Exercise> getAllExercises() {
-        return exerciseRepository.findAll()
-                .stream()
-                .filter(exercise -> !exercise.getWorkoutId().equals(Constants.CATALOG_WORKOUT_ID))
-                .toList();
-    }
-
-    public List<Exercise> getAllCatalogExercises() {
-        return exerciseRepository.findByWorkoutId(Constants.CATALOG_WORKOUT_ID);
-    }
-
-    public Exercise saveExercise(Exercise exercise) {
-        return exerciseRepository.save(exercise);
-    }
-
-    public void deleteExercise(Exercise exercise) {
-        exerciseRepository.delete(exercise);
-    }
-
-    public ExerciseSet saveExerciseSet(ExerciseSet exerciseSet) {
-        return exerciseSetRepository.save(exerciseSet);
-    }
-
-    public void deleteExerciseSet(ExerciseSet exerciseSet) {
-        exerciseSetRepository.delete(exerciseSet);
-    }
-
-    public void deleteExerciseSetById(UUID setId) {
-        exerciseSetRepository.deleteById(setId);
-    }
+    List<ExerciseDto> getCatalogExercises(Integer muscleGroupId);
+    List<ExerciseDto> getAllCatalogExercises();
+    List<ExerciseDto> getAllExercises();
+    ExerciseDto saveExercise(ExerciseDto exercise);
+    ExerciseDto updateExercise(ExerciseDto exercise);
+    void deleteExercise(ExerciseDto exercise);
 }
---
+++ src/main/java/com/koleff/kare/exercise/service/ExerciseServiceImpl.java
@@ -0,0 +1,73 @@
+package com.koleff.kare.exercise.service;
+
+import com.koleff.kare.common.Constants;
+import com.koleff.kare.exercise.mapper.ExerciseMapper;
+import com.koleff.kare.exercise.models.dto.ExerciseDto;
+import com.koleff.kare.exercise.models.entity.Exercise;
+import com.koleff.kare.exercise.models.entity.ExerciseSet;
+import com.koleff.kare.exercise.repository.ExerciseRepository;
+import com.koleff.kare.exercise.repository.ExerciseSetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ExerciseServiceImpl implements ExerciseService {
+    private final ExerciseRepository exerciseRepository;
+
+    private final ExerciseMapper exerciseMapper;
+
+    @Autowired
+    public ExerciseServiceImpl(
+            ExerciseRepository exerciseRepository,
+            ExerciseSetRepository exerciseSetRepository,
+            ExerciseMapper exerciseMapper) {
+        this.exerciseRepository = exerciseRepository;
+        this.exerciseMapper = exerciseMapper;
+    }
+
+    public List<ExerciseDto> getCatalogExercises(Integer muscleGroupId) {
+        return exerciseRepository
+                .findByWorkoutIdAndMuscleGroupId(Constants.CATALOG_WORKOUT_ID, muscleGroupId)
+                .stream()
+                .map(exerciseMapper::toDto)
+                .toList();
+    }
+
+    public List<ExerciseDto> getAllExercises() {
+        return exerciseRepository.findAll()
+                .stream()
+                .filter(exercise -> !exercise.getWorkoutId().equals(Constants.CATALOG_WORKOUT_ID))
+                .map(exerciseMapper::toDto)
+                .toList();
+    }
+
+    public List<ExerciseDto> getAllCatalogExercises() {
+        return exerciseRepository.findByWorkoutId(Constants.CATALOG_WORKOUT_ID)
+                .stream()
+                .map(exerciseMapper::toDto)
+                .toList();
+    }
+
+    public ExerciseDto saveExercise(ExerciseDto exercise) {
+        Exercise dbEntry = exerciseRepository.save(
+                exerciseMapper.toEntity(exercise)
+        );
+
+        return exerciseMapper.toDto(dbEntry);
+    }
+
+    //TODO: refactor...
+    @Override
+    public ExerciseDto updateExercise(ExerciseDto exercise) {
+        return saveExercise(exercise);
+    }
+
+    public void deleteExercise(ExerciseDto exercise) {
+        exerciseRepository.delete(
+                exerciseMapper.toEntity(exercise)
+        );
+    }
+}
