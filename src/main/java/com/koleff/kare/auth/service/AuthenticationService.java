f44271350181e3151cb76fee2bc1ed9784783da5
MartinKoleff <martin_koleff@abv.bg>
2024-05-28 00:45:51 +0300

    Migrated to requests and responses in AuthenticationController.

--- src/main/java/com/koleff/kare/auth/service/AuthenticationService.java
+++ src/main/java/com/koleff/kare/auth/service/AuthenticationService.java
@@ -1,11 +1,12 @@
 package com.koleff.kare.auth.service;
 
+import com.koleff.kare.auth.models.dto.UserDto;
 import com.koleff.kare.auth.models.entity.User;
 import com.koleff.kare.auth.models.response.AuthenticationResponse;
 
 public interface AuthenticationService {
 
-    User registerUser(String username, String password, String email);
+    UserDto registerUser(String username, String password, String email);
 
     AuthenticationResponse loginUser(String username, String password);
 
